3389f2d56468a2c3a2b6bd463b408670088477d5
brianaquino <brian.aquino50@unach.mx>
2024-08-29 15:30:32 -0600

    Agregar archivos de proyecto.

---
+++ Form_Datos/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Form_Datos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombres = tbNombre.Text;
+            string apellidos = tbApellidos.Text;
+            string edad = tbEdad.Text;
+            string estatura = tbEstatura.Text;
+            string telefono = tbTelefono.Text;
+            string genero = "";
+
+            if (rbHombre.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (rbMujer.Checked)
+            {
+                genero = "Mujer";
+            }
+
+            string datos = $"Nombres: {nombres}\r\nApellidos: {apellidos}\r\nEdad: {edad}\r\nEstatura: {estatura}\r\nTelefono: {telefono}\r\nGenero: {genero}";
+
+            string rutaArchivo = "C:/Users/briaq/OneDrive/Documents/datos.txt";
+            bool archivoExiste = File.Exists(rutaArchivo);
+
+            using (StreamWriter writer = new StreamWriter(rutaArchivo, true))
+            {
+                if (archivoExiste)
+                {
+                    writer.WriteLine();
+                }
+
+                writer.WriteLine(datos);
+            }
+
+            MessageBox.Show("Datos guardados con exito:\n\n" + datos, "Informacion",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+          
+        }
+
+     
+
+        private void btnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            tbNombre.Clear();
+            tbApellidos.Clear();
+            tbTelefono.Clear();
+            tbEdad.Clear();
+            tbEstatura.Clear();
+            rbHombre.Checked = false;
+            rbMujer.Checked = false;
+        }
+    }
+}
